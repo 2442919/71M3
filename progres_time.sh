da9cf03f6cee97e696a1169d82f5c0c3e1669f63
N151P34NU D3C384L V1093L <33691956+2442919@users.noreply.github.com>
2024-11-22 09:17:29 +0100

    Update progres_time.sh

--- progres_time.sh
+++ progres_time.sh
@@ -5,30 +5,25 @@ ora_curenta=$(date +"%H")
 minute_curente=$(date +"%M")
 secunde_curente=$(date +"%S")
 
-# Conversii pentru a preveni probleme cu numere octale
-ora_curenta=$((10#$ora_curenta))
-minute_curente=$((10#$minute_curente))
-secunde_curente=$((10#$secunde_curente))
+# Asigură-te că sunt valori numerice valide
+ora_curenta=${ora_curenta#0}         # Elimină zerourile de început
+minute_curente=${minute_curente#0}   # Elimină zerourile de început
+secunde_curente=${secunde_curente#0} # Elimină zerourile de început
 
 # Setăm lungimea barei de progres
 lungime_bara=20
 
-# Calculăm progresul pentru fiecare unitate
+# Calculează progresul
 progres_ora=$((lungime_bara * ora_curenta / 24))
 progres_minute=$((lungime_bara * minute_curente / 60))
 progres_secunde=$((lungime_bara * secunde_curente / 60))
 
-# Construim barele
-bara_ora=$(printf "%*s" $progres_ora "" | sed 's/ /█/g')
-bara_ora=$(printf "%s%*s" "$bara_ora" $((lungime_bara - progres_ora)) "" | sed 's/ /░/g')
+# Construiește barele de progres
+bara_ora=$(printf "%-${lungime_bara}s" "$(printf "%${progres_ora}s" | tr ' ' '█')" | tr ' ' '░')
+bara_minute=$(printf "%-${lungime_bara}s" "$(printf "%${progres_minute}s" | tr ' ' '█')" | tr ' ' '░')
+bara_secunde=$(printf "%-${lungime_bara}s" "$(printf "%${progres_secunde}s" | tr ' ' '█')" | tr ' ' '░')
 
-bara_minute=$(printf "%*s" $progres_minute "" | sed 's/ /█/g')
-bara_minute=$(printf "%s%*s" "$bara_minute" $((lungime_bara - progres_minute)) "" | sed 's/ /░/g')
-
-bara_secunde=$(printf "%*s" $progres_secunde "" | sed 's/ /█/g')
-bara_secunde=$(printf "%s%*s" "$bara_secunde" $((lungime_bara - progres_secunde)) "" | sed 's/ /░/g')
-
-# Afișăm rezultatele
+# Afișează rezultatele
 printf "ORA    %02d  %s\n" $ora_curenta "$bara_ora"
 printf "MINUTE %02d  %s\n" $minute_curente "$bara_minute"
 printf "SECUNDE %02d  %s\n" $secunde_curente "$bara_secunde"
